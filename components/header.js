82a441684314f78dbd208206955fe15ac0fce172
Øyvind Nordseth Westbye <oyvindnordsethwestbye@yvinds-MacBook-Pro.local>
2022-03-28 14:28:09 +0200

    First commit

---
+++ components/header.js
@@ -0,0 +1,147 @@
+
+import Link from "next/link";
+import Image from 'next/Image';
+import Logo from "public/static/logo.png";
+import styled from "styled-components";
+import { IconUser, IconSearch, IconMail } from "ui/icons";
+import { useRouter } from "next/router";
+import Search from "./search";
+
+const MyLogo = (props) => {
+  return (
+    <Image
+      src={Logo}
+      alt="Logo"
+      placeholder="blur"
+      width="173px"
+      height="40px"
+      display="block"
+    />
+  )
+};  
+
+const navHeight = "130px";
+const tints = {
+  white: "#fff",
+  black: "#000",
+  blue: "blue",
+};
+const Outer = styled.header`
+  position: fixed;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 25px 25px;
+  z-index: 100;
+  height: ${navHeight};
+  ${({ theme }) => theme.responsive.smPlus} {
+    padding: 0px 100px;
+  }
+  ${({ theme }) => theme.responsive.xs} {
+    display: grid;
+    grid-template-columns: 1r 1fr;
+    padding: 10px 20px;
+  }
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  color: ${(p) => p.color};
+  ${({ theme }) => theme.responsive.xs} {
+    height: 50px;
+  }
+  /* justify-content: stretch; */
+`;
+const NavLink = styled.span`
+  display: flex;
+  font-weight: 600;
+  cursor: pointer;
+  font-size: 15px;
+  padding: 0 15px;
+  align-items: center;
+  height: ${navHeight};
+  ${(p) =>
+    p.active &&
+    `
+  font-weight: 900;
+`}
+  ${({ theme }) => theme.responsive.mdPlus} {
+    font-size: 18px;
+    padding: 0 20px;
+  }
+  ${({ theme }) => theme.responsive.xs} {
+    padding: 0 10px;
+    &:first-child {
+      padding-left: 0;
+    }
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+`;
+const ActionBtn = styled.a`
+  height: 50px;
+  width: 50px;
+  display: flex;
+  align-items: center;
+  margin: 0 3px;
+  justify-content: center;
+  border-radius: 4px;
+  cursor: pointer;
+  border: 1px solid rgba(0, 0, 0, 0);
+  &:hover {
+    border: 1px solid rgba(0, 0, 0, 0.15);
+  }
+`;
+
+const LogoWrapper = styled.div`
+  position: relative;
+  height: 50px;
+  overflow: hidden;
+  cursor: pointer;
+  width: 250px;
+  margin: 0 auto;
+  }
+  ${({ theme }) => theme.responsive.xs} {
+    display: grid;
+    grid-row-start: 1;
+    grid-column-start: span 2;
+  }
+`;
+const Header = ({ tint }) => {
+  const router = useRouter();
+
+  const activeNav = `/${router?.asPath?.split("/")?.[1]}`;
+  return (
+    <Outer>
+      <Navigation color={tints[tint]}>
+        <Link to="/stories" href="/stories">
+          <NavLink active={activeNav === "/stories"}>Bærekraft</NavLink>
+        </Link>
+        <Link to="/om-oss" href="/om-oss">
+          <NavLink active={activeNav === "/om-oss"}>Om oss</NavLink>
+        </Link>
+      </Navigation>
+      <Link to="/" href="/">
+      <LogoWrapper>
+      <MyLogo />
+        </LogoWrapper>
+      </Link>
+      <Actions>
+        <Search>
+          <ActionBtn>
+            <IconSearch fill={tints[tint]} />
+          </ActionBtn>
+        </Search>
+        <ActionBtn as="a" href="/login">
+          <IconUser fill={tints[tint]} />
+        </ActionBtn>
+         </Actions>
+    </Outer>
+  );
+};
+
+export default Header;
---
+++ ui/icons.js
@@ -0,0 +1,66 @@
+export const IconUser = ({ fill = "#000", size = "24" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke={fill} d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+    <circle stroke={fill} cx="12" cy="7" r="4"></circle>
+  </svg>
+);
+export const IconCart = ({ fill = "#000", size = "24" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle stroke={fill} cx="9" cy="21" r="1"></circle>
+    <circle stroke={fill} cx="20" cy="21" r="1"></circle>
+    <path
+      stroke={fill}
+      d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"
+    ></path>
+  </svg>
+);
+export const IconSearch = ({ fill = "#000", size = "24" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle stroke={fill} cx="11" cy="11" r="8"></circle>
+    <line stroke={fill} x1="21" y1="21" x2="16.65" y2="16.65"></line>
+  </svg>
+);
+
+export const IconMail = ({ fill = "#000", size = "24" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg"     xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+
+<g transform="matrix(1,0,0,1,0,0)"><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M0.786999 12.3L9.781 18.86C9.92215 18.9629 10.0923 19.0183 10.267 19.0183C10.4417 19.0183 10.6118 18.9629 10.753 18.86L14.5 16.134"></path><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M7.399 17.118L1.105 23.412"></path><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M13.138 17.121L18.829 22.812"></path><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M20.27 0.500001H11.788C11.3827 0.499607 10.9813 0.579079 10.6067 0.733876C10.2322 0.888673 9.89176 1.11576 9.60499 1.40216C9.31821 1.68856 9.09069 2.02867 8.9354 2.40304C8.78012 2.77741 8.70013 3.17871 8.7 3.584V7.677C8.70026 8.49502 9.02541 9.27945 9.60393 9.85778C10.1824 10.4361 10.967 10.761 11.785 10.761H14V13.616L17.855 10.761H20.27C21.0879 10.761 21.8724 10.4361 22.4507 9.85772C23.0291 9.27936 23.354 8.49493 23.354 7.677V3.584C23.354 2.76607 23.0291 1.98165 22.4507 1.40328C21.8724 0.824922 21.0879 0.500001 20.27 0.500001V0.500001Z"></path><path stroke="#000000" d="M12.173 6.02899C11.9659 6.02899 11.798 5.8611 11.798 5.65399C11.798 5.44688 11.9659 5.27899 12.173 5.27899"></path><path stroke="#000000" d="M12.173 6.02899C12.3801 6.02899 12.548 5.8611 12.548 5.65399C12.548 5.44688 12.3801 5.27899 12.173 5.27899"></path><path stroke="#000000" d="M16.029 6.02899C15.8219 6.02899 15.654 5.8611 15.654 5.65399C15.654 5.44688 15.8219 5.27899 16.029 5.27899"></path><path stroke="#000000" d="M16.029 6.02899C16.2361 6.02899 16.404 5.8611 16.404 5.65399C16.404 5.44688 16.2361 5.27899 16.029 5.27899"></path><path stroke="#000000" d="M19.885 6.02899C19.6779 6.02899 19.51 5.8611 19.51 5.65399C19.51 5.44688 19.6779 5.27899 19.885 5.27899"></path><path stroke="#000000" d="M19.885 6.02899C20.0921 6.02899 20.26 5.8611 20.26 5.65399C20.26 5.44688 20.0921 5.27899 19.885 5.27899"></path><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M18.829 14.149V22.674C18.829 22.7827 18.8075 22.8904 18.7658 22.9908C18.7241 23.0912 18.663 23.1824 18.586 23.2592C18.509 23.3359 18.4176 23.3967 18.317 23.438C18.2165 23.4793 18.1087 23.5004 18 23.5H1.472C1.25293 23.5 1.04284 23.413 0.88793 23.2581C0.733025 23.1032 0.646001 22.8931 0.646001 22.674V12.756C0.645869 12.6475 0.667137 12.5399 0.708589 12.4396C0.75004 12.3393 0.810862 12.2481 0.887576 12.1713C0.964291 12.0945 1.05539 12.0336 1.15567 11.992C1.25595 11.9504 1.36344 11.929 1.472 11.929H7.46"></path></g></svg>
+);
